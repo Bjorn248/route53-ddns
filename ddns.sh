9b7cd781799f41080ff3f8706b1a6da08add8067
Bjorn Stange <bjorn248@gmail.com>
2014-08-18 10:55:43 -0400

    made domain another parameter instead of giving my domain in the file statically :P

--- ddns.sh
+++ ddns.sh
@@ -11,6 +11,7 @@ CURRENT_IP=$(curl checkip.amazonaws.com)
 if [ "$CURRENT_RECORD" != "$CURRENT_IP" ];
 then
     sed -r "s/FILL_ME_IN/$CURRENT_IP/g" update_template.json > update.json
+    sed -ri "s/FILL_IN_WITH_DOMAIN_YOU_WISH_TO_BE_DYNAMIC/$MY_DOMAIN/g" update.json
     aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file://./update.json
     echo "DNS Updated"
 else
